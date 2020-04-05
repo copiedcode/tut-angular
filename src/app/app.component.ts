00794a67c38016a8b337c55498923179eb3a4ff6
copiedcode <raphael.webdesign@outlook.com>
2020-04-05 21:45:46 +0200

    Doing initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{ title }} </h1>
+  `
 })
-export class AppComponent {
-  title = 'tut-angular';
+
+export class AppComponent implements OnInit {
+  title: string;
+  constructor(){
+
+  }
+  ngOnInit(){
+    //Service Call
+    this.title = 'Hello World';
+  }
 }
