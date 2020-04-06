b272ab438350c15a3fba26cf266122b924409a93
copiedcode <raphael.webdesign@outlook.com>
2020-04-06 16:07:16 +0200

    Add customers module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent, CustomersComponent ],
+  imports: [ BrowserModule ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
