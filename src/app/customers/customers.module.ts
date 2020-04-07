2bfc1aa9e1af9166e1aaf50cfd0e3935d8cb7e3c
copiedcode <raphael.webdesign@outlook.com>
2020-04-07 17:59:40 +0200

    add filter function

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,13 +1,15 @@
 import { NgModule }      from '@angular/core';
+import { FormsModule }      from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { CustomersComponent }  from './customers.component';
 import { CustomersListComponent } from './customers-list/customers-list.component'; 
 import { FilterTextboxComponent } from './customers-list/filter-textbox.component'; 
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  imports:      [ CommonModule ],
+  imports:      [ CommonModule, SharedModule, FormsModule ],
   declarations: [ CustomersComponent, CustomersListComponent, FilterTextboxComponent ],
   exports: [ CustomersComponent ]
 })
